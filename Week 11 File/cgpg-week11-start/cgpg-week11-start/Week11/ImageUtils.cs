3e2ff5bbc53a42414d6ee0c97aeccbb18f773bf1
WeiEnZero <shutman89@gmail.com>
2025-06-30 11:04:45 +0800

    Updated GreyScale code

--- Week 11 File/cgpg-week11-start/cgpg-week11-start/Week11/ImageUtils.cs
+++ Week 11 File/cgpg-week11-start/cgpg-week11-start/Week11/ImageUtils.cs
@@ -57,9 +57,9 @@ namespace CGPG
             byte[] output = new byte[input.Length];
             for (int i = 0; i < input.Length; i += 4) // RGBA
             {
-                output[i] = 255;  //Red
-                output[i + 1] = 255; //Green
-                output[i + 2] = 255; //Blue
+                output[i] = 0;  //Red
+                output[i + 1] = 0; //Green
+                output[i + 2] = 0; //Blue
                 output[i + 3] = input[i + 3]; // Keep Alpha
             }
 
