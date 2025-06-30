b755621cb8aa372ac4cf9b5cb87013a813f0d2d2
WeiEnZero <shutman89@gmail.com>
2025-06-30 10:15:25 +0800

    Added Week 2 File

---
+++ Week 2/Assets/Cube.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //this.transform.localScale = new Vector3(3, 1, 2);
+        //this.transform.localEulerAngles = new Vector3(45, 45, 0);
+        this.transform.position = new Vector3(0, 1, -2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
